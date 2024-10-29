98670e8713ac46cac004db36f0a04ee2bee87c15
Eleana <markoueleana@gmail.com>
2024-10-29 20:11:51 +0200

    add builing block cqrs

--- eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -2,7 +2,8 @@
 
 namespace Catalog.API.Products.CreateProduct
 {
-    public record CreateProductCommand( string Name ,List<string> Category, string Description, string ImageFile,decimal Price):IRequest<CreateProductResult>;
+    public record CreateProductCommand( string Name ,List<string> Category, string Description, string ImageFile,decimal Price)
+        :IRequest<CreateProductResult>;
     public record CreateProductResult(Guid Id);
     internal class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
     {
