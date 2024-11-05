22fcf2a548999e50cde83a96e7daefce22008067
Eleana <markoueleana@gmail.com>
2024-11-05 21:18:14 +0200

    add GetProductByCategory

---
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,23 @@
+﻿using Marten.Linq.QueryHandlers;
+
+namespace Catalog.API.Products.GetProductByCategoryId
+{
+    public record GetProductByCategoryQuery (string Category) : IQuery<GetProductByCategoryResult>;
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+    internal class GetProductByCategoryQueryHandler 
+        (IDocumentSession session, ILogger<GetProductByCategoryResult> logger)
+        : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByCategoryQueryHandler.Handler called with {@Query}", query);
+
+            var products = await session.Query<Product>()
+                .Where(p => p.Category.Contains(query.Category))
+                .ToListAsync();
+
+            return new GetProductByCategoryResult(products);
+
+        }
+    }
+}
