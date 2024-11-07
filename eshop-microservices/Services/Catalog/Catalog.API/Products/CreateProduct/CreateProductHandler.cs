e06cb0109b6e86ec24c92a32f604947c6261e2d2
Eleana <markoueleana@gmail.com>
2024-11-07 23:04:43 +0200

    add fluent Validator in CreateProduct endpoint

--- eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,30 +1,47 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
 {
-    public record CreateProductCommand( string Name ,List<string> Category, string Description, string ImageFile,decimal Price)
-        :ICommand<CreateProductResult>;
-    public record CreateProductResult(Guid Id);
-    internal class CreateProductHandler (IDocumentSession session)
-        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    public CreateProductCommandValidator()
     {
-        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+        RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+    }
+}
+internal class CreateProductHandler(IDocumentSession session, IValidator<CreateProductCommand> validator)
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        var result = await validator.ValidateAsync(command, cancellationToken);
+        var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
+
+        if (errors.Any())
         {
-            //create product entity from incoming command object
-            var product = new Product
-            {
-                Name = command.Name,
-                Category = command.Category,
-                Description = command.Description,
-                ImageFile = command.ImageFile,
-                Price = command.Price
-            };
-
-            //save to the database
-            session.Store(product);
-            await session.SaveChangesAsync(cancellationToken);
-
-            //return CreateProductResult result
-            return new CreateProductResult(product.Id);
-            throw new NotImplementedException();
+            throw new ValidationException(errors.FirstOrDefault());
         }
+        //create product entity from incoming command object
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        //save to the database
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+
+        //return CreateProductResult result
+        return new CreateProductResult(product.Id);
+        throw new NotImplementedException();
     }
 }
--- eshop-microservices/Services/Catalog/Catalog.API/Program.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddMarten(opts =>
     opts.Connection(builder.Configuration.GetConnectionString("Database"!));
 }).UseLightweightSessions();
 
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+
 var app = builder.Build();
 
 //Configura the HTTP request pipeline
