1abf1dfca4d21541a921518d72574b6f26f2ad14
Eleana <markoueleana@gmail.com>
2024-11-07 22:25:15 +0200

    add DeleteProduct endpoint

---
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,29 @@
+﻿
+using Catalog.API.Products.GetProducts;
+
+namespace Catalog.API.Products.DeleteProduct
+{
+    //public record DeleteProductRequest();
+
+   public record DeleteProductResponse(bool isSuccess);
+    public class DeleteProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/products/{id}", async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteProductCommand(id));
+
+                var response = result.Adapt(result);
+
+                return Results.Ok(response);
+            })
+                .WithName("DeleteProduct")
+                .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .ProducesProblem(StatusCodes.Status404NotFound)
+                .WithSummary("Delete Product")
+                .WithDescription("Delete Product");
+        }
+    }
+}
--- eshop-microservices/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -5,7 +5,7 @@ namespace Catalog.API.Products.UpdateProduct
         :ICommand<UpdateProductResult>;
 
     public record UpdateProductResult(bool IsSuccess);
-    internal class UpdateProductCommandHandler(IDocumentSession session, ILogger<UpdateProductResult> logger)
+    internal class UpdateProductCommandHandler(IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
         : ICommandHandler<UpdateProductCommand, UpdateProductResult>
     {
         public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
