1814d1ea795b74a56228be83bc0b2f961e81321a
Eleana <markoueleana@gmail.com>
2024-11-12 21:54:55 +0200

    add validators to CRUD opetations

--- eshop-microservices/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -7,6 +7,14 @@ namespace Catalog.API.Products.DeleteProduct
     public record DeleteProductCommand( Guid Id) : ICommand<DeleteProductResult>;
 
     public record DeleteProductResult(bool IsSuccess);
+
+    public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
+    { 
+        public DeleteProductCommandValidator()
+        {
+            RuleFor(command => command.Id).NotEmpty().WithMessage("Prouct ID is required"); 
+        }
+    }
     internal class DeleteProductCommandHandler
         (IDocumentSession session, ILogger<DeleteProductCommandHandler> logger)
         : ICommandHandler<DeleteProductCommand, DeleteProductResult>
--- eshop-microservices/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -1,10 +1,26 @@
 ﻿
+using Catalog.API.Products.CreateProduct;
+
 namespace Catalog.API.Products.UpdateProduct
 {
     public record UpdateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price)
         :ICommand<UpdateProductResult>;
 
     public record UpdateProductResult(bool IsSuccess);
+    public class UpdateCommandValidator : AbstractValidator<UpdateProductCommand>
+    {
+        public UpdateCommandValidator()
+        {
+            RuleFor(command => command.Id).NotEmpty().WithMessage("Product ID is required");
+               
+            RuleFor(command => command.Name)
+                .NotEmpty().WithMessage("Product Name is required")
+                .Length(2,250).WithMessage("Name must be between 2 and 150 characters");
+
+            RuleFor(command => command.Price)
+                .GreaterThan(0).WithMessage("Product's Price must be greater then 0");
+        }
+    }
     internal class UpdateProductCommandHandler(IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
         : ICommandHandler<UpdateProductCommand, UpdateProductResult>
     {
