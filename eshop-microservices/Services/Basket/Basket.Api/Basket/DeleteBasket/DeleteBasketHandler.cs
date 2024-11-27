1f4d10989962582e4f0c4da0a23be8af403ea161
Eleana <markoueleana@gmail.com>
2024-11-27 22:36:47 +0200

    delete operation

--- eshop-microservices/Services/Basket/Basket.Api/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ eshop-microservices/Services/Basket/Basket.Api/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -1,6 +1,23 @@
-﻿namespace Basket.Api.Basket.DeleteBasket
+﻿
+namespace Basket.Api.Basket.DeleteBasket;
+
+public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketCommandResult>;
+
+public record DeleteBasketCommandResult (bool IsSuccess);
+
+public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand> 
 {
-    public class DeleteBasketHandler
+    public DeleteBasketCommandValidator()
     {
+        RuleFor(x => x.UserName).NotEmpty().WithMessage("The username shound not be empty!");
+    }
+}
+public class DeleteBasketCommandHandler : ICommandHandler<DeleteBasketCommand, DeleteBasketCommandResult>
+{
+    public async Task<DeleteBasketCommandResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+    {
+        //todo : delete basket from db and cache
+        //
+        return new DeleteBasketCommandResult(true);
     }
 }
